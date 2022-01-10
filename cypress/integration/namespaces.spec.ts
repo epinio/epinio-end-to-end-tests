80664d21065666ab89568173cc21237a6124a361
Loic Devulder <ldevulder@suse.com>
2022-01-10 16:52:29 +0100

    Move tests to functions
    This is helpful for multiple Helm scenarii tests, to avoid duplicate
    too much code.

--- cypress/integration/namespaces.spec.ts
+++ cypress/integration/namespaces.spec.ts
@@ -5,9 +5,6 @@ Cypress.config();
 describe('Namespace testing', () => {
   const topLevelMenu = new TopLevelMenu();
   const epinio = new Epinio();
-  const defaultNamespace = 'workspace';
-  const namespace = 'mynamespace';
-  const appName = 'testapp';
 
   beforeEach(() => {
     cy.login();
@@ -15,28 +12,12 @@ describe('Namespace testing', () => {
     topLevelMenu.openIfClosed();
     epinio.accessEpinioMenu(Cypress.env('cluster'));
   });
-  
-  it('Create namespace', () => {
-    cy.createNamespace(namespace);
-  });
 
   it('Push and check an application into the created namespace', () => {
-    cy.createApp({appName: appName, archiveName: 'sample-app.tar.gz'});
-    cy.checkApp({appName: appName, namespace: namespace});
-  });
-
-  it('Delete namespace', () => {
-    cy.deleteNamespace({namespace: namespace, appName: appName});
+    cy.runNamespaceTest('newNamespace');
   });
 
   it('Try to push an application without any namespace', () => {
-    // Delete default namespace
-    cy.deleteNamespace({namespace: defaultNamespace});
-
-    // Try to create the application
-    cy.createApp({appName: appName, archiveName: 'sample-app.tar.gz', shouldBeDisabled: true});
-
-    // Re-create default namespace
-    cy.createNamespace(defaultNamespace);
+    cy.runNamespaceTest('withoutNamespace');
   });
 });
