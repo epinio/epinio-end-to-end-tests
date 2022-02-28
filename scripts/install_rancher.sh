fc20f1d6e857e6b10f5b1edeac8432f58e520628
Loic Devulder <ldevulder@suse.com>
2022-02-28 14:07:24 +0100

    Small fixes in Rancher installation
    Signed-off-by: Loic Devulder <ldevulder@suse.com>

--- scripts/install_rancher.sh
+++ scripts/install_rancher.sh
@@ -12,27 +12,28 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
 # Cert Manager has to be installed before Rancher
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.1/cert-manager.crds.yaml
 kubectl create namespace cattle-system
-helm install cert-manager jetstack/cert-manager \
+helm upgrade --install cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.5.1 \
+  --set installCRDs=true \
+  --set extraArgs[0]=--enable-certificate-owner-ref=true \
   --wait
 
 # Wait for cert-manager deployment to be ready
 kubectl rollout status deployment cert-manager -n cert-manager --timeout=120s
 
 # Install Rancher
-helm install rancher rancher-stable/rancher \
- --namespace cattle-system \
- --set hostname=${MY_HOSTNAME} \
- --set bootstrapPassword=rancherpassword \
- --set "extraEnv[0].name=CATTLE_UI_DASHBOARD_INDEX" \
- --set "extraEnv[0].value=https://releases.rancher.com/dashboard/${DASHBOARD_VERSION}/index.html" \
- --set "extraEnv[1].name=CATTLE_UI_OFFLINE_PREFERRED" \
- --set "extraEnv[1].value=Remote" \
- --wait
+helm upgrade --install rancher rancher-stable/rancher \
+  --namespace cattle-system \
+  --create-namespace \
+  --set hostname=${MY_HOSTNAME} \
+  --set bootstrapPassword=rancherpassword \
+  --set "extraEnv[0].name=CATTLE_UI_DASHBOARD_INDEX" \
+  --set "extraEnv[0].value=https://releases.rancher.com/dashboard/${DASHBOARD_VERSION}/index.html" \
+  --set "extraEnv[1].name=CATTLE_UI_OFFLINE_PREFERRED" \
+  --set "extraEnv[1].value=Remote" \
+  --wait
 
 # Wait for rancher deployment to be ready
 kubectl rollout status deployment rancher -n cattle-system --timeout=300s
