117a6f9ee38dc0ed87641c069b553a1b10555d10
Julien ADAMEK <julien.adamek@suse.com>
2022-06-23 16:49:07 +0200

    Switch to an official docker hub account

--- scripts/patch_epinio-ui.sh
+++ scripts/patch_epinio-ui.sh
@@ -3,4 +3,4 @@
 set -ex
 
 # Patch the epinio-ui deployment with latest QA image
-kubectl set image -n epinio deployment/epinio-ui epinio-ui=juadk/epinio-ui-qa:latest
+kubectl set image -n epinio deployment/epinio-ui epinio-ui=epinioteam/epinio-ui-qa:latest
