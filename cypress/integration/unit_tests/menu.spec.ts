3c29169573a66bf984b5582fca39950d54516a0a
mmartin24 <manuel.martin@suse.com>
2022-07-08 17:23:18 +0200

    Placing login with different users test in menu.spec.ts

--- cypress/integration/unit_tests/menu.spec.ts
+++ cypress/integration/unit_tests/menu.spec.ts
@@ -48,3 +48,39 @@ describe('Menu testing', () => {
     cy.contains(defaultNamespace).should('be.visible');
   }
 )});
+
+// // Note: this test may need to be adapted with Rancher Dashboard
+describe('Login with different users', () => {
+
+  it('Check login with admin user', () => {
+    const user_epinio = "admin"
+    const pwd_epinio = "password"
+    cy.login(user_epinio, pwd_epinio);
+    cy.contains('Invalid username or password. Please try again.').should('not.exist')
+    cy.contains('Applications').should('be.visible')
+  });
+
+  it('Check login with regular user and simple password', () => {
+    const user_epinio = "epinio"
+    const pwd_epinio = "password"
+    cy.login(user_epinio, pwd_epinio);
+    cy.contains('Invalid username or password. Please try again.').should('not.exist')
+    cy.contains('Applications').should('be.visible')
+  });
+
+  it('Check login with regular user "user1" and password with special characters', () => {
+    const user_epinio = "user1"
+    const pwd_epinio = "Hell@World"
+    cy.login(user_epinio, pwd_epinio);
+    cy.contains('Invalid username or password. Please try again.').should('not.exist')
+    cy.contains('Applications').should('be.visible')
+  });
+
+  it('Check login with regular user "user2" and password with many special characters', () => {
+    const user_epinio = "user2"
+    const pwd_epinio = "Hell#@~%/=World"
+    cy.login(user_epinio, pwd_epinio);
+    cy.contains('Invalid username or password. Please try again.').should('not.exist')
+    cy.contains('Applications').should('be.visible')
+  });
+})
