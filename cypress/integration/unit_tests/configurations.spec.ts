49d8c20a0e875275de7e7c795a39c0590c513792
Manuel Martín <manuel.martin@suse.com>
2022-09-27 15:28:00 +0200

    skipping most tests for checking report quickly

--- cypress/integration/unit_tests/configurations.spec.ts
+++ cypress/integration/unit_tests/configurations.spec.ts
@@ -2,7 +2,7 @@ import { Epinio } from '~/cypress/support/epinio';
 import { TopLevelMenu } from '~/cypress/support/toplevelmenu';
 
 Cypress.config();
-describe('Configuration testing', () => {
+describe.skip('Configuration testing', () => {
   const topLevelMenu = new TopLevelMenu();
   const epinio = new Epinio();
 
