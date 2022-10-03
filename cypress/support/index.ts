c84b9dfaba9e331231ffa284b8bbf64e3f0b770e
Manuel Martín <manuel.martin@suse.com>
2022-10-03 14:50:36 +0200

    Adding step copy screenshots and videos to mochawesome and adapting function

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -61,11 +61,14 @@ Cypress.on('uncaught:exception', (err, runnable, promise) => {
 
 Cypress.on('test:after:run', (test, runnable) => {
   if (test.state === 'failed') {
-      const screenshot = `../cypress/screenshots/${Cypress.spec.name
+      // const screenshot = `../cypress/screenshots/${Cypress.spec.name
+      // }/${runnable.parent.title} -- ${test.title} (failed).png`;
+      const screenshot = `screenshots/${Cypress.spec.name
       }/${runnable.parent.title} -- ${test.title} (failed).png`;
       addContext({ test }, screenshot);
-      const video = `../cypress/videos/${Cypress.spec.name
-      }.mp4`;
+      
+      // const video = `../cypress/videos/${Cypress.spec.name}.mp4`;
+      const video = `videos/${Cypress.spec.name}.mp4`;
       addContext({ test }, video);
   }
 });
