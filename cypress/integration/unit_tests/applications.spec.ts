697e4557a3e5e503ab2ae8c43b3ae4e707e1b237
Manuel Martín <manuel.martin@suse.com>
2022-10-14 16:52:47 +0200

    removing only test from applictaions left

--- cypress/integration/unit_tests/applications.spec.ts
+++ cypress/integration/unit_tests/applications.spec.ts
@@ -18,7 +18,7 @@ describe('Applications testing', () => {
     cy.deleteAllConfigurations()
   });
 
-  it.only('Push basic application and check we can restart and rebuild it', () => {
+  it('Push basic application and check we can restart and rebuild it', () => {
     cy.runApplicationsTest('restartAndRebuild');
   });
 
