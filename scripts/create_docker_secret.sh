1a73a88ca3bd6a13d73cb11bc9b714feb95a11a9
Tomas Hehejik <thehejik@suse.com>
2022-11-11 14:17:34 +0100

    Assign regcred secret to default sa

--- scripts/create_docker_secret.sh
+++ scripts/create_docker_secret.sh
@@ -7,3 +7,5 @@ kubectl create secret docker-registry regcred \
   --docker-server https://index.docker.io/v1/ \
   --docker-username $EXT_REG_USER \
   --docker-password $EXT_REG_PASSWORD
+# Use the docker hub credentials in default sa
+kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
