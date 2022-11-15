d161959ef9a45416a75c8f923338cfb5a895f080
Tomas Hehejik <thehejik@suse.com>
2022-11-15 10:34:17 +0100

    Use epinio-ui:latest-next image for tests

--- scripts/patch_epinio-ui.sh
+++ scripts/patch_epinio-ui.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
-# Patch the epinio-ui deployment with latest QA image
-kubectl set image -n epinio deployment/epinio-ui epinio-ui=epinioteam/epinio-ui-qa:latest
+# Patch the epinio-ui deployment with latest dev image
+# The image is building by https://github.com/epinio/ui-backend/actions/workflows/release-next.yml
+kubectl set image -n epinio deployment/epinio-ui epinio-ui=ghcr.io/epinio/epinio-ui:latest-next
