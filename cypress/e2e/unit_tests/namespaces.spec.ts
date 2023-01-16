d3e55abc8e72adfe29d4097089164fe8371896a1
Manuel Martín <manuel.martin@suse.com>
2023-01-16 14:31:37 +0100

    adding tags and reducing test execution on namespace test for checking implementation of test grep

--- cypress/e2e/unit_tests/namespaces.spec.ts
+++ cypress/e2e/unit_tests/namespaces.spec.ts
@@ -15,7 +15,7 @@ describe('Namespaces testing', () => {
     }
   });
 
-  it('Push and check an application into the created namespace', () => {
+  it('Push and check an application into the created namespace', { tags: '@ns-1' }, () => {
     cy.runNamespacesTest('newNamespace');
   });
 
