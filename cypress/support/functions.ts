e6805612e6185bf62e98a52eac79051fd0cd7344
Manuel Martín <manuel.martin@suse.com>
2023-05-11 16:33:40 +0200

    changing sso label

--- cypress/support/functions.ts
+++ cypress/support/functions.ts
@@ -58,7 +58,7 @@ Cypress.Commands.add('login', (username = Cypress.env('username'), password = Cy
 Cypress.Commands.add('dexLogin', (username = 'admin@epinio.io', password = 'password', checkLandingPage = true ) => {
   // Dex connection. Enter username/pwd
   cy.visit('/auth/login')
-  cy.get('.btn.bg-primary').contains('Log in with Dex').should('be.visible').click({force : true});
+  cy.get('.btn.bg-primary').contains('Log in with SSO').should('be.visible').click({force : true});
   // Log into Dex Account
   cy.get('input#login', {timeout: 5000}).should('be.visible').focus().type(username);
   cy.get('input#password', {timeout: 5000}).should('be.visible').focus().type(password);
