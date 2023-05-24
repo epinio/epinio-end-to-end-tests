c0f90b641cf8408451424a492a91d53f119b3a89
Manuel Martín <manuel.martin@suse.com>
2023-05-24 10:23:57 +0200

    adding more time in gitlab tests

--- cypress/support/functions.ts
+++ cypress/support/functions.ts
@@ -218,14 +218,17 @@ Cypress.Commands.add('loadGitRepo', ({ gitUsername, gitRepo, gitBranch, gitCommi
   cy.get('.labeled-input.edit.has-tooltip > input[type="text"]',{timeout:5000}).focus().clear().type(gitUsername,{delay:250, force:true})
 
   // Selecting Repository
+  cy.wait(1000)
   cy.contains('label', 'Repository ').should('be.visible').click();
-  cy.contains(gitRepo).click();
+  cy.contains(gitRepo,{timeout:5000}).click();
 
   // Selecting Branch
+  cy.wait(1000)
   cy.contains('label', 'Branch').should('be.visible').click();
   cy.contains(gitBranch,{timeout:5000}).should('be.visible').click();
   
   // Selecting commit based on commit name
+  cy.wait(1000)
   cy.get(`tr[data-node-id=${gitCommit}] > td`, {timeout:5000}).eq(0).should('be.visible').click();
 });
 
