749fb02037044d54a19b052036a1989624bd8e9e
Tomas Hehejik <thehejik@suse.com>
2023-09-06 09:33:52 +0200

    Always (re)pull epinio-ui:latest-next (#373)

--- scripts/patch_epinio-ui.sh
+++ scripts/patch_epinio-ui.sh
@@ -3,6 +3,7 @@
 set -ex
 
 # Patch the epinio-ui deployment with latest dev image
-# The image is building by https://github.com/epinio/ui-backend/actions/workflows/release-next.yml
+# The image is building by https://github.com/epinio/ui/actions/workflows/release-next.yml
 kubectl set image -n epinio deployment/epinio-ui epinio-ui=ghcr.io/epinio/epinio-ui:latest-next
+kubectl patch deployment epinio-ui -n epinio -p '{"spec":{"template":{"spec":{"containers":[{"name":"epinio-ui", "imagePullPolicy":"Always"}]}}}}'
 kubectl wait pods -n epinio -l app.kubernetes.io/name=epinio-ui --for=condition=ready --timeout=2m
