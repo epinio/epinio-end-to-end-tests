7631a53892517f371135333bca5c5ac9e9f21c98
Manuel Martín <manuel.martin@suse.com>
2023-09-20 17:07:46 +0200

    Cypress/update ep version command (#384)
    * adding epinio repo addition for a custom version
    
    * updating patch script to disable its use if epinio version param is passed.
    
    * updating wf to extend env var epinio version to patch script.
    Adding echo message in patch script

--- scripts/patch_epinio-ui.sh
+++ scripts/patch_epinio-ui.sh
@@ -2,9 +2,14 @@
 
 set -ex
 
-# Patch the epinio-ui deployment with latest dev image
-# The image is building by https://github.com/epinio/ui/actions/workflows/release-next.yml
-kubectl set image -n epinio deployment/epinio-ui epinio-ui=ghcr.io/epinio/epinio-ui:latest-next
-kubectl wait pods -n epinio -l app.kubernetes.io/name=epinio-ui --for=condition=ready --timeout=2m
-kubectl patch deployment epinio-ui -n epinio -p '{"spec":{"template":{"spec":{"containers":[{"name":"epinio-ui", "imagePullPolicy":"Always"}]}}}}'
-kubectl wait pods -n epinio -l app.kubernetes.io/name=epinio-ui --for=condition=ready --timeout=2m
+# If epinio version is not passed as parameter, then patch image
+if [[ -z $EPINIO_VERSION  ]]; then
+  # Patch the epinio-ui deployment with latest dev image
+  # The image is building by https://github.com/epinio/ui/actions/workflows/release-next.yml
+  kubectl set image -n epinio deployment/epinio-ui epinio-ui=ghcr.io/epinio/epinio-ui:latest-next
+  kubectl wait pods -n epinio -l app.kubernetes.io/name=epinio-ui --for=condition=ready --timeout=2m
+  kubectl patch deployment epinio-ui -n epinio -p '{"spec":{"template":{"spec":{"containers":[{"name":"epinio-ui", "imagePullPolicy":"Always"}]}}}}'
+  kubectl wait pods -n epinio -l app.kubernetes.io/name=epinio-ui --for=condition=ready --timeout=2m
+else
+  echo "Skipping patching, using upstream chart $EPINIO_VERSION"
+fi
